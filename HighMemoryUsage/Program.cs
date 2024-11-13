4e530b98f7ff3258a9f8fd0e92a32922383990b5
Yana Makogon <yana.mak89@gmail.com>
2024-11-13 19:32:04 +0100

    updated .gitignore added Program.cs

---
+++ HighMemoryUsage/Program.cs
@@ -0,0 +1,15 @@
+﻿using HighMemoryUsage;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        using (var largeObject = new LargeMemoryObject(100_000_000, "example.txt"))
+        {
+            largeObject.InitializeArray();
+            Console.WriteLine("Елемент масиву: " + largeObject.GetElement(888));
+        }
+
+        Console.WriteLine("Об'єкт видалено після виходу з блоку using.");
+    }
+}
---
+++ MemoryMonitoringApp/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        long maxMemoryUsage = 500 * 1024 * 1024;
+        using (var monitor = new MemoryMonitoring(maxMemoryUsage))
+        {
+            monitor.MemoryThresholdReached += (sender, e) =>
+            {
+                Console.WriteLine("Використання пам'яті наблизилось до критичного рівня!");
+            };
+            Console.WriteLine("Розпочато моніторинг ресурсів...");
+            Console.WriteLine("Програма очікує поки користувач натисне Enter");
+            Console.ReadLine(); 
+        }
+
+        Console.WriteLine("Моніторинг завершено.");
+    }
+}
